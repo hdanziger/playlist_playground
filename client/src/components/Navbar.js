2051051d10d3e9bd220e82bc4118450808a04dce
github username <github email address>
2018-04-02 12:07:25 -0400

    src removal

--- client/src/components/Navbar.js
+++
@@ -1,7 +0,0 @@
-
-//   <header className="App-header">
-//     <h1 className="App-title">Welcome to the Playlist Playground!</h1>
-//   </header>
-//   <p className="App-intro">
-//   </p>
-// </div>
