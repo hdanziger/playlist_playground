f213ec4dc0f288882fffa65ffa2eb909a5dd2341
github username <github email address>
2018-04-02 16:04:38 -0400

    playlist table update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-playlists = Playlist.create( title: 'Todays Top Hits', genre: "pop", songs: "Groovy songs!", img_url: "")
-playlists = Playlist.create( title: 'Slow Jams', genre: "rock", songs: "Oldies but goodies", img_url: "")
-songs = Song.create( title: "Here comes the sun", artist: "The Beatles", playlist_id: 1)
-songs = Song.create( title: "Sweet Home Alabama", artist: "Lynyrd Skynyrd", playlist_id: 1)
-songs = Song.create( title: "Yellow", artist: "Coldplay", playlist_id: 2)
+Playlist.create( title: 'Todays Top Hits', genre: "pop", img_url: "")
+Playlist.create( title: 'Slow Jams', genre: "rock", img_url: "")
+Song.create( title: "Here comes the sun", artist: "The Beatles", playlist_id: 1)
+Song.create( title: "Sweet Home Alabama", artist: "Lynyrd Skynyrd", playlist_id: 1)
+Song.create( title: "Yellow", artist: "Coldplay", playlist_id: 2)
