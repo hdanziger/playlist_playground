39136b42f281e6ca7255e552d93fc4ebc084d645
github username <github email address>
2018-04-04 13:02:40 -0400

    remove songs controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     namespace :api do
     resources :playlists, except: [:new, :edit]
-    resources :songs, except: [:new, :show, :edit]
   end
 end
