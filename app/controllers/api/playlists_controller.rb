e3565ef2e1761b49cd15bce6af70e41eefd11188
github username <github email address>
2018-04-05 14:14:28 -0400

    fix destroy path

--- app/controllers/api/playlists_controller.rb
+++ app/controllers/api/playlists_controller.rb
@@ -1,6 +1,6 @@
 class Api::PlaylistsController < ApplicationController
 
-  before_action :set_playlist, only: [:edit, :show, :update, :destory]
+  before_action :set_playlist, only: [ :show, :destroy]
 
   def index
     render json: Playlist.all
@@ -19,15 +19,8 @@ class Api::PlaylistsController < ApplicationController
     render json: @playlist
   end
 
-  def update
-    if @playlist.update(playlist_params)
-      render json: @playlist
-    else
-      render json: {message: @playlist.errors }, status: 400
-    end
-  end
 
-  def destory
+  def destroy
     if @playlist.destroy
       render status: 204
     else
